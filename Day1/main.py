df3de3dc3426f868555efcba4a7a54cf4db8182d
thanhtunggc92 <nguyentruonggthanhtung@gmail.com>
2022-04-22 14:37:51 -0700

    init

---
+++ Day1/main.py
@@ -0,0 +1,8 @@
+#1 Create a greeting for your program
+print("***************Hello*******************")
+
+city= input("What city do you grow up?")
+
+pet_name=input("What is you pet's name?")
+
+print("Your branch name could be" + city + " "+ pet_name)
